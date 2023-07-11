8fda617fbed15225f5208d8228f4edc1b38d98f4
syr <syr0024@hanyang.ac.kr>
2023-07-11 21:10:53 +0900

    update network_R with quat

--- src/network/losses.py
+++ src/network/losses.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from network.covariance_parametrization import DiagonalParam
+from lietorch import SO3, LieGroupParameter
 from utils.lie_algebra import so3_log
 from utils.from_scipy import compute_q_from_matrix
 from utils.math_utils import logdet3, sixD2so3, so32sixD
@@ -11,24 +12,50 @@ MIN_LOG_STD = np.log(1e-3)
 SO(3) loss
 
 input: 
-  pred: Nx6 tensor of network orientation 1,2 column of orientation matrix output
-  targ: batchx3x3xN tensor of gt 1,2 column of orientation matrix
+  pred: batchx6x(N) tensor of network orientation 1,2 column of orientation matrix output
+  targ: batchx3x3x(N) tensor of gt 1,2 column of orientation matrix
 output:
-  loss: Nx6 matrix of SO(3) loss on rotation matrix ?
+  loss: batchx(N) matrix of SO(3) loss on rotation matrix ?
 """
 
+def loss_L2dis_so3(pred, targ):
+    M = targ * pred.transpose(1,2)
+    loss = torch.acos_(0.5*(M[:, 0, 0]+M[:, 1, 1]+M[:, 2, 2]-1))
+    return loss
 
 def loss_mse_so3(pred, targ):
 
     # pres, targ is torch.tensor
-    if pred.dim() < 4:
-        pred = pred.unsqueeze(3)
-        targ = targ.unsqueeze(3)
+    # if pred.dim() < 4:
+    #     pred = pred.unsqueeze(3)
+    #     targ = targ.unsqueeze(3)
 
-    loss = (pred - targ).pow(2).squeeze()   # tensor(1024,3,3)
-    # 아래 코드로 하면 pred과 targ 사이의 각도차이가 작아지면서 nan 값이 나와 오류 발생
+    ## 기존 displacement loss 계산 방식
+    # loss = (pred - targ).pow(2).squeeze()   # tensor(1024,3,3)
+
+    ## lie_algebra so3_log 사용:  pred과 targ 사이의 각도차이가 작아지면서 nan 값이 나와 오류 발생
     # loss = so3_log(pred.transpose(1, 2).bmm(targ).squeeze()).pow(2).squeeze()   # tensor(1024,3)
     # loss = loss.unsqueeze(2).transpose(1, 2).bmm(loss.unsqueeze(2)).squeeze()  # tensor(1024,)
+
+    ## lietorch: log를 계산할 때 batch의 크기가 768을 넘어가면 768번째부터 nan이 생겨서 분할해줌
+    if pred.size()[0] > 767:
+        n = int(pred.size()[0] / 2)
+        M1 = SO3(pred[:n,:,:].transpose(1,2).bmm(targ[:n,:,:]))
+        M2 = SO3(pred[n:,:,:].transpose(1,2).bmm(targ[n:,:,:]))
+        M = torch.cat((M1.log(), M2.log()), dim=0).norm(dim=-1).unsqueeze(2)
+    else:
+        M = SO3(pred.transpose(1,2).bmm(targ))
+        M = M.log().norm(dim=-1).unsqueeze(2)
+
+    loss = loss.transpose(1,2).bmm(loss).squeeze()
+
+    # # nan 디버깅:
+    # indices = torch.isnan(loss).nonzero()
+    # first_nan_index = indices[0]
+    #
+    # print("처음으로 NaN이 나타나는 인덱스:", first_nan_index)
+    # print(loss[first_nan_index[0]-1,:,:])
+    # print(M.data[first_nan_index[0],:,:])
 
     return loss
 
@@ -48,11 +75,25 @@ def loss_NLL_so3(pred, pred_cov, targ):
     pred_cov = pred_cov.squeeze()
     targ = targ.squeeze()
 
-    loss = (pred - targ).pow(2) / (2 * torch.exp(2 * pred_cov)) + pred_cov
-    # 아래 코드로 하면 pred과 targ 사이의 각도차이가 작아지면서 nan 값이 나와 오류 발생
+    ## 기존 displacement loss 계산 방식
+    # loss = (pred - targ).pow(2) / (2 * torch.exp(2 * pred_cov)) + pred_cov
+
+    ## lie_algebra so3_log 사용: pred과 targ 사이의 각도차이가 작아지면서 nan 값이 나와 오류 발생
     # residual = so3_log(pred.bmm(targ.transpose(1,2))).unsqueeze(2)
     # weighted_term = 0.5 * residual.transpose(1,2).bmm(pred_cov).bmm(residual)
     # loss = weighted_term.squeeze() - 0.5 * torch.log((pred_cov[:, 0, 0]*pred_cov[:, 1, 1]*pred_cov[:, 2, 2])**2)
+
+    ## lietorch
+    if pred.size()[0] > 767:
+        n = int(pred.size()[0] / 2)
+        M1 = SO3(pred[:n,:,:].transpose(1,2).bmm(targ[:n,:,:]))
+        M2 = SO3(pred[n:,:,:].transpose(1,2).bmm(targ[n:,:,:]))
+        M = torch.cat((M1.log(), M2.log()), dim=0).norm(dim=-1).unsqueeze(2)
+    else:
+        M = SO3(pred.transpose(1,2).bmm(targ))
+        M = M.log().norm(dim=-1).unsqueeze(2)
+
+    loss = 0.5*M.transpose(1,2).bmm(pred_cov).bmm(M).squeeze() - 0.5*torch.log((pred_cov[:, 0, 0]*pred_cov[:, 1, 1]*pred_cov[:, 2, 2])**2)
 
     return loss
 
@@ -146,7 +187,7 @@ def get_loss(pred, pred_logstd, targ, epoch):
 
 def get_loss_so3(pred, pred_logstd, targ, epoch):
 
-    if epoch < 10:
+    if epoch < 0:
         loss = loss_mse_so3(pred, targ)
     else:
         loss = loss_NLL_so3(pred, pred_logstd, targ)
